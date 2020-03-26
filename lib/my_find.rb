2e5c6719e76fe88098c8373c040aef858d431ea1
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 22:57:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length do
+    if yield(collection[i]) == collection[i]
+      collection[i]
+    end
+    i += 1
+  end
 end
